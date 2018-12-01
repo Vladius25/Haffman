2b60bf8486867fbc1f8e3c2b8029801f1ceacef8
Vladius25 <vkorol2509@icloud.com>
2018-12-01 12:35:50 +0500

    Optimization and code refactoring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import heapq
 import pickle
+import time
 import __hello__
 
 
@@ -29,10 +30,37 @@ class Tree:
         return "({}, {}, {})".format(self.left, self.right, self.parent)
 
 
+def timing(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        output = func(*args, **kwargs)
+        print(func.__name__ + ": %s" % (time.time() - start_time))
+        return output
+
+    return wrapper
+
+
 def get_min():
     return heapq.heappop(forest), heapq.heappop(forest)
 
 
+@timing
+def make_forest():
+    global tree
+    vals = dict(Counter(input))
+    for ch in range(256):
+        try:
+            root = len(forest)
+            forest.append(Forest(weight=vals[ch], root=root))
+            roots[root] = ch
+        except KeyError:
+            continue
+
+    tree = [Tree() for _ in range(len(forest))]
+    heapq.heapify(forest)
+
+
+@timing
 def make_tree():
     while len(forest) > 1:
         left, right = get_min()
@@ -42,15 +70,20 @@ def make_tree():
         tree[left.root].parent = tree[right.root].parent = root
 
 
+@timing
 def make_codes(root, code=''):
-    left, right = tree[root].left, tree[root].right
-    if -1 in (right, left):
-        codes[roots[root]] = code
-        return
-    make_codes(left, code + '0')
-    make_codes(right, code + '1')
+    def dfs(root, code=''):
+        left, right = tree[root].left, tree[root].right
+        if -1 in (right, left):
+            codes[roots[root]] = code
+            return
+        dfs(left, code + '0')
+        dfs(right, code + '1')
+
+    dfs(root, code)
 
 
+@timing
 def make_binstring():
     str = ''
     for ch in input:
@@ -58,15 +91,17 @@ def make_binstring():
     return str
 
 
+@timing
 def encode():
     pickle.dump(tree, f2)
 
     output = make_binstring()
-    #f2.write(bytes(int(output[i: i + 8], 2) for i in range(0, len(output), 8)))
+    f2.write(bytes(int(output[i: i + 8], 2) for i in range(0, len(output), 8)))
 
 
+# @timing
 # def encode2():
-#     write_tree()
+#     pickle.dump(tree, f2)
 #
 #     sym, length = 0, 0
 #     bytes_list = []
@@ -77,29 +112,20 @@ def encode():
 #             if length == 8:
 #                 bytes_list.append(sym)
 #                 sym, length = 0, 0
-#     if(length < 8):
+#     if length < 8:
 #         bytes_list.append(sym)
 #     f2.write(bytes(bytes_list))
-
 
 
 if __name__ == "__main__":
     f1 = open("input.txt", "rb")
     f2 = open("input.rar", "wb")
+    input = f1.read()
     # f1.write('a'*12+'b'*40+'c'*15+'d'*8+'e'*25)
 
-    forest, roots, codes = [], {}, {}
+    forest, roots, codes, tree = [], {}, {}, []
 
-    input = f1.read()
-    vals = Counter(sorted(input)).items()
-    for root, syms in enumerate(vals):
-        forest.append(Forest(weight=syms[1], root=root))
-        roots[root] = syms[0]
-
-    tree = [Tree() for _ in range(len(forest))]
-    heapq.heapify(forest)
-
+    make_forest()
     make_tree()
     make_codes(len(tree) - 1)
     encode()
-
